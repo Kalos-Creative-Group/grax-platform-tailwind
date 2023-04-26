fa1355e5f2c653d650c88efb142a0778817cdce0
Alex Knoll <alex@kaloscreativegroup.com>
2023-04-26 16:30:12 -0400

    reset and purge placeholder content

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,10 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['*.html', '_site/*.html','_site/**/*.html', '_pages/**/*.md', '_posts/**/*.md', '_layouts/**/*.html', '_includes/**/*.html'],
   theme: {
-    extend: {
-      colors: {
-        gray: colors.slate,
-        primary: colors.rose,
-        secondary: colors.indigo,
-        tertiary: colors.slate,
-        danger: colors.red,
-      },
-      fontFamily: {
-        sans: ['Work Sans','Inter var', ...defaultTheme.fontFamily.sans]
-      }
-    }
+    extend: {}
   },
   variants: {
     extend: {}
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio')
-  ]
+  plugins: []
 }
